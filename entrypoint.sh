feee719a98099d289556ded8e30e7877c18ae432
Divyank Shah <divyank.shah.2016@gmail.com>
2023-12-20 00:47:11 -0800

    debugging

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,6 +12,7 @@ for FILE in $(find ./public -name '*.png' -or -name '*.jpg' -or -name '*jpeg'); 
     cwebp  $FILE -o $NEW
 
     WEBP=${FILEPATH/$EXT/webp}
+    echo $FILEPATH $WEBP
     sed -i '' -e "s/$FILEPATH/$WEBP/g" `find ./src -name '*.jsx' -or -name '*.js' -or -name '*tsx' -or -name '*.ts'`
 
     rm $FILE
