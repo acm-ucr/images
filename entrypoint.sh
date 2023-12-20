afb4153039129cca740430afe2177d9a626af6b1
Divyank Shah <divyank.shah.2016@gmail.com>
2023-12-19 23:55:29 -0800

    setup action

---
+++ entrypoint.sh
@@ -0,0 +1,14 @@
+for FILE in $(find ./public -name '*.png' -or -name '*.jpg' -or -name '*jpeg'); do 
+    FILEPATH=$(basename $FILE)
+    
+    FILENAME=${FILEPATH%.*}
+    EXT="${FILEPATH##*.}"
+
+    NEW=${FILE/$EXT/webp}
+    cwebp -quiet $FILE -o $NEW
+
+    WEBP=${FILEPATH/$EXT/webp}
+    sed -i '' -e "s/$FILEPATH/$WEBP/g" `find ./src -name '*.jsx' -or -name '*.js'`
+
+    rm $FILE
+done
