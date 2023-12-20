917b8df44edcabf9ad45be1595543d1ce7b8c92b
Divyank Shah <divyank.shah.2016@gmail.com>
2023-12-20 01:36:30 -0800

    cleanup

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,18 +12,6 @@ for FILE in $(find ./public -name '*.png' -or -name '*.jpg' -or -name '*jpeg'); 
     cwebp $FILE -o $NEW
 
     WEBP=${FILEPATH/$EXT/webp}
-    echo $FILEPATH $WEBP $(find ./src -name '*.jsx' -or -name '*.js')
     sed -i -e "s/$FILEPATH/$WEBP/g" `find ./src -name '*.jsx' -or -name '*.js' -or -name '*tsx' -or -name '*.ts'`
-    ls
-    echo "public files"
-    cd public
-    ls
-    cd ..
-    echo "src files"
-    cd src
-    ls
-    cat Test.jsx
-    cd ..
-    echo $FILE
     rm $FILE
 done
